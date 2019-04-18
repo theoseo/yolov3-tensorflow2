68be364c11f39d90e695561392698e6aaea6ba76
Suhong Seo <suhong.seo@gmail.com>
2019-04-18 05:57:11 +0000

    modify convert_weights for tensorflow 2.0

---
+++ convert_weights.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import yolo_v3
+import logging
+import argparse
+
+from utils import load_coco_names, load_weights
+
+parser = argparse.ArgumentParser(description="YOLO-V3 weight convert")
+
+parser.add_argument("--class_names", type=str, default="coco.names",
+                    help="File with class names.")    
+parser.add_argument("--weights_file", type=str, default="yolov3.weights",
+                    help="Binary file with detector weights.")    
+parser.add_argument("--data_format", type=str, default="channels_last",
+                    help="Data format: channels_first / channels_last.")    
+#parser.add_argument("--tiny", type=bool, default=False,
+#                    help="Use tiny version of YOLOv3.")    
+parser.add_argument("--tf2_weights", type=str, default="./tf2_weights/yolov3",
+                    help="Tensorflow 2.0 Weights file.")    
+
+
+def main(argv=None):
+
+    classes = load_coco_names(args.class_names)
+    inputs = tf.keras.Input(shape=(416,416,3))
+
+    yolov3 = yolo_v3.YoloV3(len(classes), data_format=args.data_format)
+    detections = yolov3(inputs, training=False)
+    load_ops = load_weights(yolov3, args.weights_file)  
+
+    """
+    Saving Subclassed Models
+
+    https://www.tensorflow.org/alpha/guide/keras/saving_and_serializing
+    Sequential models and Functional models are datastructures that represent a DAG of layers. 
+    As such, they can be safely serialized and deserialized.
+
+    https://medium.com/tensorflow/what-are-symbolic-and-imperative-apis-in-tensorflow-2-0-dfccecb01021
+    """  
+    yolov3.save_weights(args.tf2_weights)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
